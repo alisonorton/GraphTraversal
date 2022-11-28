42a9118aa0970a1d0da7fb77a3c19723b9db1c17
alisonorton <orton.alison@gmail.com>
2022-11-28 12:50:05 -1000

    Creating Main file. Reading and parsing data

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+
+    //private HashMap<String, Node> nodes;
+
+    public Main(){
+        // nodes = new HashMap<>(); 
+        // Scanner s = new Scanner(System.in);
+        // String[] nodeNames = s.nextLine().split(" ");
+        // String starterKey = s.nextLine();
+
+        // for(var key : nodeNames){
+        //     Node n = new Node(key);
+        //     nodes.put(key, n);
+        // }
+
+        // Node startNode = nodes.get(starterKey);
+
+        // System.out.println(nodes);
+    }
+    public static void main(String[] args) {
+        HashMap<String, Node> nodes;
+        nodes = new HashMap<>(); 
+        Scanner s = new Scanner(System.in);
+        String[] nodeNames = s.nextLine().split(" ");
+        String starterKey = s.nextLine();
+
+        for(var key : nodeNames){
+            Node n = new Node(key);
+            nodes.put(key, n);
+        }
+
+        Node startNode = nodes.get(starterKey);
+
+        while(true){
+            String a = s.next();
+            if(a.equals("***")){
+                break;
+            }
+            Node n1 = nodes.get(a);
+            String b = s.next();
+            Node n2 = nodes.get(b);
+            n1.addNeighbor(n2);
+            n2.addNeighbor(n1);
+
+        }
+        s.close();
+
+        for(String key : nodes.keySet()){
+            //Print neighbors
+            System.out.println(nodes.keySet());
+        }
+    }
+}
+
